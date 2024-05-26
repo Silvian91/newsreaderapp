62694ad16eae866b6fbb20edcda528b7d5b45cad
Silvian91 <silvian.rosianu@yahoo.com>
2024-05-27 00:18:38 +0200

    provide loadnews repository

--- app/src/main/java/com/example/newsreaderapp/di/NewsModule.kt
+++ app/src/main/java/com/example/newsreaderapp/di/NewsModule.kt
@@ -1,11 +1,13 @@
 package com.example.newsreaderapp.di
 
 import com.example.newsreaderapp.data.GetNewsRepositoryImpl
+import com.example.newsreaderapp.data.LoadNewsRepositoryImpl
 import com.example.newsreaderapp.data.database.dao.ArticlesDao
 import com.example.newsreaderapp.data.model.EntityToModelMapper
 import com.example.newsreaderapp.data.model.ResponseToEntityMapper
 import com.example.newsreaderapp.domain.NewsApi
 import com.example.newsreaderapp.domain.repository.GetNewsRepository
+import com.example.newsreaderapp.domain.repository.LoadNewsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,12 +19,20 @@ class NewsModule {
 
     @Provides
     fun provideGetNewsRepository(
-        newsApi: NewsApi,
         articlesDao: ArticlesDao,
         entityToModelMapper: EntityToModelMapper,
-        responseToEntityMapper: ResponseToEntityMapper,
+        loadNewsRepository: LoadNewsRepository
         ): GetNewsRepository {
-        return GetNewsRepositoryImpl(newsApi,articlesDao, entityToModelMapper, responseToEntityMapper)
+        return GetNewsRepositoryImpl(articlesDao, entityToModelMapper, loadNewsRepository)
+    }
+
+    @Provides
+    fun provideLoadNewsRepository(
+        newsApi: NewsApi,
+        articlesDao: ArticlesDao,
+        responseToEntityMapper: ResponseToEntityMapper,
+    ): LoadNewsRepository {
+        return LoadNewsRepositoryImpl(newsApi, articlesDao, responseToEntityMapper)
     }
 
     @Provides
