3b41f76205008b6077b4d01ba7c51f541f7c2425
Silvian91 <silvian.rosianu@yahoo.com>
2024-05-26 20:51:00 +0200

    add app database

---
+++ app/src/main/java/com/example/newsreaderapp/data/database/model/ArticlesEntity.kt
@@ -0,0 +1,17 @@
+package com.example.newsreaderapp.data.database.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+data class ArticlesEntity(
+    @PrimaryKey val id: String,
+    val sourceName: String?,
+    val author: String?,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String?,
+    val publishedAt: String,
+    val content: String
+)
