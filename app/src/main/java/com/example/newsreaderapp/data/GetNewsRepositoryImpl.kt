1489bbb9faa62020d7b567cc240fd810808bab17
Silvian91 <silvian.rosianu@yahoo.com>
2024-05-27 00:17:33 +0200

    move api call and database storing to loadNews

--- app/src/main/java/com/example/newsreaderapp/data/GetNewsRepositoryImpl.kt
+++ app/src/main/java/com/example/newsreaderapp/data/GetNewsRepositoryImpl.kt
@@ -6,29 +6,20 @@ import com.example.newsreaderapp.data.model.ResponseToEntityMapper
 import com.example.newsreaderapp.data.model.NewsModel
 import com.example.newsreaderapp.domain.NewsApi
 import com.example.newsreaderapp.domain.repository.GetNewsRepository
+import com.example.newsreaderapp.domain.repository.LoadNewsRepository
 import kotlinx.coroutines.flow.firstOrNull
 
 class GetNewsRepositoryImpl(
-    private val newsApi: NewsApi,
     private val articlesDao: ArticlesDao,
     private val entityToModelMapper: EntityToModelMapper,
-    private val responseToEntityMapper: ResponseToEntityMapper
+    private val loadNewsRepository: LoadNewsRepository
 ) : GetNewsRepository {
     override suspend fun getNews(): NewsModel {
         val articlesFromDb = articlesDao.getArticles().firstOrNull()
         return if (!articlesFromDb.isNullOrEmpty()) {
             NewsModel("ok", articlesFromDb.map { entityToModelMapper.map(it) })
         } else {
-            val response = newsApi.getNews()
-            if (response.status == "ok") {
-                articlesDao.deleteAll()
-                articlesDao.insertArticles(response.articles.map {
-                    responseToEntityMapper.map(it)
-                })
-                response.toModel()
-            } else {
-                throw Exception("Failed to fetch news")
-            }
+            loadNewsRepository.loadNews()
         }
     }
 }
